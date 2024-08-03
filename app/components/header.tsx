9fa48da69ab12797469cbaf30831c3bd983fdd47
priyankastro <priyankastro7@gmail.com>
2024-08-03 20:16:01 +0530

    logo test

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -10,17 +10,17 @@ const DynamicUserProfileButton = dynamic(() => import('../sub-components/user-pr
 
 const Header = () => {
   return (
-    <header className="bg-white relative z-20 border-gray-200 sticky top-0">
+    <header style={{backgroundColor: "rgb(26, 26, 26)"}} className="relative z-20 border-gray-200 sticky top-0">
       <nav className="max-w-screen-xl mx-auto px-3">
-        <div className="flex flex-wrap items-center justify-between mx-auto">
+        <div className="flex flex-wrap items-center justify-between mx-auto py-3">
           <a href="/" className="mx-2 md:mx-4 flex items-center space-x-3 h-[80px]">
-            <span className="self-center text-2xl font-semibold whitespace-nowrap">LOGO</span>
-            {/* <img src="/images/chandapura.png" className="w-full h-full" /> */}
+            {/* <span className="self-center text-2xl font-semibold whitespace-nowrap">LOGO</span> */}
+            <img src="/images/logo3.png" className="w-full h-full" />
           </a>
           <div className="flex items-center">
             <ul className="hidden md:flex font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:mt-0 md:border-0 md:bg-white">
               <li>
-                <a href={Resource.BusinessListing.link} className="link-pill">{Resource.BusinessListing.label}</a>
+                <a href={Resource.BusinessListing.dashboardLink} className="link-pill">{Resource.BusinessListing.label}</a>
               </li>
               <li>
                 <a href={Resource.Advertisement.link} className="link-pill">{Resource.Advertisement.label}</a>

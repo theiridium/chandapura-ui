c4309b1534b6947937ff44caeb5c90c175f08b19
priyankastro <priyankastro7@gmail.com>
2024-08-12 20:53:51 +0530

    multi-image bug fix

--- app/components/media-upload-input/multi-image.tsx
+++ app/components/media-upload-input/multi-image.tsx
@@ -108,8 +108,8 @@ const MultiImage = ({ imageParams, uploadSuccess }: any) => {
     return (
         <>
             <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 mb-6">
-                {existingImageBlock}
-                {newImgPreview}
+                {existingFiles.length > 0 && existingImageBlock}
+                {files.length > 0 && newImgPreview}
                 <div {...getRootProps({ className: 'w-full' })}>
                     <div className="flex flex-col items-center justify-center w-full aspect-square border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
                         <div className="flex flex-col items-center justify-center pt-5 pb-6">

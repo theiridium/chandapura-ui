b65662b8c7f7acbe6b40e6a2b20bb9e990bdc2b2
priyankastro <priyankastro7@gmail.com>
2024-08-18 00:18:24 +0530

    view loc map updated

--- app/components/maps/view-location-map.tsx
+++ app/components/maps/view-location-map.tsx
@@ -1,24 +1,17 @@
 "use client"
-import React, { useEffect, useRef } from 'react';
-const ViewLocationMap = ({coordinates}: any) => {
-  const mapRef: any = useRef(null);
-  useEffect(() => {
-    const googleMapsScript = document.createElement('script');
-    googleMapsScript.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAP_API}`;
-    googleMapsScript.async = true;
-    googleMapsScript.defer = true;
-    googleMapsScript.addEventListener('load', initializeMap);
-    document.body.appendChild(googleMapsScript);
-  }, []);
-  const initializeMap = () => {
-    const map = new window.google.maps.Map(mapRef.current, {
-      center: { lat: coordinates.lat, lng: coordinates.lng }, // Default to San Francisco
-      zoom: 20,
-    });
-    // Add your location-based features here
-  };
+
+const ViewLocationMap = ({ coordinates }: any) => {
+
   return (
-    <div className='google-map' ref={mapRef} style={{ height: '400px', width: '100%' }}></div>
+    <iframe
+      width="100%"
+      height="450"
+      className="rounded-lg"
+      referrerPolicy="no-referrer-when-downgrade"
+      src={`https://www.google.com/maps?q=${coordinates.lat},${coordinates.lng}&hl=en&z=20&amp&output=embed`}
+    >
+    </iframe>
   );
 };
+
 export default ViewLocationMap;

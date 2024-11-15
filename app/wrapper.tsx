edb564b023d7bc40e9de514b8017188ae7ab8f8d
priyankastro <priyankastro7@gmail.com>
2024-11-15 14:27:25 +0530

    update

--- app/wrapper.tsx
+++ app/wrapper.tsx
@@ -36,7 +36,7 @@ const Wrapper = ({
           <div>{children}</div>
           {/* WhatsApp Icon */}
           <div className="w-full flex justify-end sticky bottom-1">
-            <a aria-label="Chat on WhatsApp" href="https://wa.me/9739004545"> <img className="h-16 w-16" alt="Chat on WhatsApp" src="/images/icons/WhatsApp.svg" /></a>
+            <a aria-label="Chat on WhatsApp" target="_blank" href="https://wa.me/9739004545"> <img className="h-16 w-16" alt="Chat on WhatsApp" src="/images/icons/WhatsApp.svg" /></a>
           </div>
           <Footer />
         </Provider>

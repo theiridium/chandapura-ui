0e38e6b1a9e6995ad02175d90eb016427b54714b
priyankastro <priyankastro7@gmail.com>
2024-12-07 06:45:50 +0530

    sidemenu updated with nextui upgrade

--- app/sub-components/menubar.tsx
+++ app/sub-components/menubar.tsx
@@ -1,87 +1,66 @@
 import { Resource } from '@/public/shared/app.config'
-import { motion } from 'framer-motion'
-import { X } from 'lucide-react'
+import { Drawer, DrawerBody, DrawerContent, DrawerHeader } from '@nextui-org/react'
 import { signOut, useSession } from 'next-auth/react'
 
 const Menubar = (props: any) => {
     const { status, data }: any = useSession();
-    const var_menu = {
-        hidden: {
-            opacity: 0,
-            x: '100vw',
-            duration: 2,
-            ease: "linear",
-            transition: { type: 'tween' }
-        },
-        visible: {
-            opacity: 1,
-            x: 0,
-            duration: 2,
-            ease: "linear",
-            transition: { type: 'tween' }
-        },
-        bgHidden: {
-            opacity: 0,
-            duration: 2,
-            ease: "linear"
-        },
-        bgVisible: {
-            opacity: 1,
-            duration: 2,
-            ease: "linear"
-        }
-    }
     return (
-        <motion.div className='sidemenubar fixed w-screen h-screen top-0 right-0 bg-overlay/50' variants={var_menu} initial="bgHidden" animate="bgVisible" exit="bgHidden">
-            <motion.div className='h-screen fixed w-full md:w-96 bg-color1d text-white absolute top-0 right-0 p-10 overflow-auto' variants={var_menu} initial="hidden" animate="visible" exit="hidden">
-                {status === "authenticated" &&
+        <Drawer
+            isOpen={props.isOpen}
+            onClose={props.onClose}
+            classNames={{ base: "bg-color1d text-white", closeButton: "text-color2d hover:text-color1d text-lg font-bold" }}
+            size='sm'
+            radius='none'>
+            <DrawerContent>
+                {(onClose) => (
                     <>
-                        <div className='text-2xl mb-2'>Welcome back</div>
-                        <div className='text-3xl font-semibold'>{data?.user?.firstname} !</div>
+                        <DrawerHeader className="flex flex-col gap-1 border-0">
+                            <div className='text-2xl mb-2 font-medium'>Welcome back</div>
+                            <div className='text-3xl font-medium'>{status === "authenticated" ? data?.user?.firstname : "Guest"} !</div>
+                        </DrawerHeader>
+                        <DrawerBody>
+                            <ul className="text-2xl my-12 gap-10">
+                                {status === "unauthenticated" &&
+                                    <li className='mb-10'>
+                                        <a className='hover-underline' href={Resource.Login.link}>{Resource.Login.label}</a>
+                                    </li>
+                                }
+                                {status === "authenticated" &&
+                                    <>
+                                        <li className='mb-10'>
+                                            <a className='hover-underline' href={Resource.MyProfile.link}>{Resource.MyProfile.label}</a>
+                                        </li>
+                                        <li className='mb-10'>
+                                            <a className='hover-underline' href={Resource.Dashboard.link}>{Resource.Dashboard.label}</a>
+                                        </li>
+                                    </>
+                                }
+                                <li className='mb-10'>
+                                    <a className='hover-underline' href={Resource.Advertisement.dashboardLink}>{Resource.Advertisement.label}</a>
+                                </li>
+                                <li className='mb-10'>
+                                    <a className='hover-underline' href={Resource.BusinessListing.dashboardLink}>{Resource.BusinessListing.label}</a>
+                                </li>
+                                <li className='mb-10'>
+                                    <a className='hover-underline' href={Resource.PropertyListing.dashboardLink}>{Resource.PropertyListing.label}</a>
+                                </li>
+                                <li className='mb-10'>
+                                    <a className='hover-underline' href={Resource.ClassifiedListing.dashboardLink}>{Resource.ClassifiedListing.label}</a>
+                                </li>
+                                <li className='mb-10'>
+                                    <a className='hover-underline' href={Resource.JobListing.dashboardLink}>{Resource.JobListing.label}</a>
+                                </li>
+                                {status === "authenticated" &&
+                                    <li className='mb-10'>
+                                        <button className='bg-red-500 text-white py-2 px-4' onClick={() => signOut({ callbackUrl: '/' })}>{Resource.Logout.label}</button>
+                                    </li>
+                                }
+                            </ul>
+                        </DrawerBody>
                     </>
-                }
-                <button className='absolute right-7 top-5 hover:text-color2d' onClick={() => props.btnToggle(!props.isOpen)}>
-                    <X strokeWidth={3} size={34} />
-                </button>
-                <ul className="text-2xl my-12 gap-10">
-                    {status === "unauthenticated" &&
-                        <li className='mb-10'>
-                            <a className='hover-underline' href={Resource.Login.link}>{Resource.Login.label}</a>
-                        </li>
-                    }
-                    {status === "authenticated" &&
-                        <>
-                            <li className='mb-10'>
-                                <a className='hover-underline' href={Resource.MyProfile.link}>{Resource.MyProfile.label}</a>
-                            </li>
-                            <li className='mb-10'>
-                                <a className='hover-underline' href={Resource.Dashboard.link}>{Resource.Dashboard.label}</a>
-                            </li>
-                        </>
-                    }
-                    <li className='mb-10'>
-                        <a className='hover-underline' href={Resource.Advertisement.dashboardLink}>{Resource.Advertisement.label}</a>
-                    </li>
-                    <li className='mb-10'>
-                        <a className='hover-underline' href={Resource.BusinessListing.dashboardLink}>{Resource.BusinessListing.label}</a>
-                    </li>
-                    <li className='mb-10'>
-                        <a className='hover-underline' href={Resource.PropertyListing.dashboardLink}>{Resource.PropertyListing.label}</a>
-                    </li>
-                    <li className='mb-10'>
-                        <a className='hover-underline' href={Resource.ClassifiedListing.dashboardLink}>{Resource.ClassifiedListing.label}</a>
-                    </li>
-                    <li className='mb-10'>
-                        <a className='hover-underline' href={Resource.JobListing.dashboardLink}>{Resource.JobListing.label}</a>
-                    </li>
-                    {status === "authenticated" &&
-                        <li className='mb-10'>
-                            <button className='bg-purple-950 text-red-500 py-2 px-4' onClick={() => signOut({ callbackUrl: '/' })}>{Resource.Logout.label}</button>
-                        </li>
-                    }
-                </ul>
-            </motion.div>
-        </motion.div>
+                )}
+            </DrawerContent>
+        </Drawer>
     )
 }
 
--- app/sub-components/user-profile-button.tsx
+++ app/sub-components/user-profile-button.tsx
@@ -1,52 +1,30 @@
 "use client"
 import React from 'react'
-import { useEffect, useState } from 'react';
 import { User, X } from 'lucide-react';
-import { AnimatePresence, motion } from 'framer-motion';
 import Menubar from './menubar';
 import { useSession } from 'next-auth/react';
+import { useDisclosure } from '@nextui-org/react';
 
 const UserProfileButton = () => {
-    const [btnToggle, setBtnToggle] = useState(false);
-    const toggle = (x: boolean) => setBtnToggle(x);
     const { data }: any = useSession();
     const userData = data?.user;
-
-    useEffect(() => {
-        if (btnToggle) {
-            // Add styles to prevent scrolling when menu is open
-            document.documentElement.style.paddingRight = "0px";
-            document.documentElement.style.overflow = "hidden";
-        } else {
-            // Reset styles when menu is closed
-            document.documentElement.style.paddingRight = "";
-            document.documentElement.style.overflow = "";
-        }
-
-        // Cleanup on component unmount
-        return () => {
-            document.documentElement.style.paddingRight = "";
-            document.documentElement.style.overflow = "";
-        };
-    }, [btnToggle]);
+    const {isOpen, onOpen, onClose} = useDisclosure();
 
     return (
         <>
             {userData && userData.image ?
-                <button className='mx-2 md:mx-4 border-2 border-color1d hover:border-color2d rounded-full' onClick={() => setBtnToggle(!btnToggle)} type="button" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                <button className='mx-2 md:mx-4 border-2 border-color1d hover:border-color2d rounded-full' onClick={() => onOpen()} type="button" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                     <span className="sr-only">Open user menu</span>
                     <img src={userData.image} className='h-[44px] w-[44px] md:h-[48px] md:w-[48px] rounded-full' />
                 </button> :
-                <button className='mx-2 md:mx-4 p-2 bg-color2d rounded-full text-color1d border-2 border-color2d' onClick={() => setBtnToggle(!btnToggle)} type="button" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                <button className='mx-2 md:mx-4 p-2 bg-color2d rounded-full text-color1d border-2 border-color2d' onClick={() => onOpen()} type="button" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                     <span className="sr-only">Open user menu</span>
                     <User strokeWidth={2.2} fill='#ffc901' className='h-[24px] w-[24px]' />
                 </button>
             }
-            <AnimatePresence initial={false}>
-                {btnToggle &&
-                    <Menubar btnToggle={toggle} isOpen={btnToggle} />
-                }
-            </AnimatePresence>
+            <div>
+                <Menubar isOpen={isOpen} onClose={onClose} />
+            </div>
         </>
     )
 }

fc1e25b6da1c65eafc5df98dc055a044ad25611e
priyankastro <priyankastro7@gmail.com>
2025-01-03 09:24:25 +0530

    design update in search

--- app/sub-components/search-bar.tsx
+++ app/sub-components/search-bar.tsx
@@ -67,8 +67,17 @@ const SearchBar = ({ productType }: any) => {
     <form className="search-container" onSubmit={onSearch}>
       <div className="search-input" role="combobox" aria-haspopup="listbox" >
         <input className="w-full" aria-controls="suggest-box" placeholder="Start searching..." onChange={inputHandler} value={text} onFocus={() => setIsFocused(true)} onBlur={() => setTimeout(() => setIsFocused(false), 200)} />
-        {isFocused && suggest.length > 0 && <div id="suggest-box" role="listbox" className="search-list">
-          {suggest.map((x: any, i: any) => <div onClick={() => onSuggestClick(x.name)} className="py-2 px-5 hover:bg-default-100 rounded-md cursor-pointer" key={i}>{x.name}</div>)}
+        {suggest.length > 0 && <div id="suggest-box" role="listbox" className="search-list">
+          {isFocused &&
+            suggest.map((x: any, i: any) =>
+              <div onClick={() => onSuggestClick(x.name)} className="py-3 px-5 hover:bg-default-100 rounded-md cursor-pointer flex items-start gap-x-3 font-semibold" key={i}>
+                <Search className="mt-1" size={20} />
+                <div>
+                  <div>{x.name}</div>
+                  <div className="text-sm font-light">Category</div>
+                </div>
+              </div>
+            )}
         </div>}
       </div>
       {!!text && <button type="button" className="bg-default-100 w-10 aspect-square text-center rounded-full hover:bg-default-200" onClick={() => setSearchText("")}><X className="m-auto" size={18} strokeWidth={2} /></button>}

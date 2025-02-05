e4e6f61348c848fe6195fda14daaff64e15f4c47
priyankastro <priyankastro7@gmail.com>
2025-02-06 05:16:28 +0530

    update

--- app/sub-components/user-profile-button.tsx
+++ app/sub-components/user-profile-button.tsx
@@ -8,15 +8,20 @@ import { useDisclosure } from '@nextui-org/react';
 const UserProfileButton = () => {
     const { data }: any = useSession();
     const userData = data?.user;
-    const {isOpen, onOpen, onClose} = useDisclosure();
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
     return (
         <>
-            {userData && userData.image ?
-                <button className='mx-2 md:mx-4 border-2 border-color1d hover:border-color2d rounded-full' onClick={() => onOpen()} type="button" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
-                    <span className="sr-only">Open user menu</span>
-                    <img src={userData.image} className='h-[44px] w-[44px] md:h-[48px] md:w-[48px] rounded-full' />
-                </button> :
+            {userData ?
+                !!userData.image ?
+                    <button className='mx-2 md:mx-4 border-2 border-color1d hover:border-color2d rounded-full' onClick={() => onOpen()} type="button" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                        <span className="sr-only">Open user menu</span>
+                        <img src={userData.image} className='h-[44px] w-[44px] md:h-[48px] md:w-[48px] rounded-full' />
+                    </button> :
+                    <button className='mx-2 md:mx-4 p-2 bg-color2d rounded-full text-color1d border-2 border-color2d' onClick={() => onOpen()} type="button" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                        <span className="sr-only">Open user menu</span>
+                        <div className='h-[24px] w-[24px] text-xl font-medium leading-tight uppercase'>{userData?.firstname[0]}</div>
+                    </button> :
                 <button className='mx-2 md:mx-4 p-2 bg-color2d rounded-full text-color1d border-2 border-color2d' onClick={() => onOpen()} type="button" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                     <span className="sr-only">Open user menu</span>
                     <User strokeWidth={2.2} fill='#ffc901' className='h-[24px] w-[24px]' />

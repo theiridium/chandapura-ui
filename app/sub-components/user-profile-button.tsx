e162d666cb052e2848187bbc6444ce053d00b433
priyankastro <priyankastro7@gmail.com>
2025-02-06 05:36:45 +0530

    menu update

--- app/sub-components/user-profile-button.tsx
+++ app/sub-components/user-profile-button.tsx
@@ -18,9 +18,9 @@ const UserProfileButton = () => {
                         <span className="sr-only">Open user menu</span>
                         <img src={userData.image} className='h-[44px] w-[44px] md:h-[48px] md:w-[48px] rounded-full' />
                     </button> :
-                    <button className='mx-2 md:mx-4 p-2 bg-color2d rounded-full text-color1d border-2 border-color2d' onClick={() => onOpen()} type="button" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                    <button className='mx-2 md:mx-4 p-2 bg-color2d text-color1d border-2 border-color1d hover:border-color2d rounded-full' onClick={() => onOpen()} type="button" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                         <span className="sr-only">Open user menu</span>
-                        <div className='h-[24px] w-[24px] text-xl font-medium leading-tight uppercase'>{userData?.firstname[0]}</div>
+                        <div className='h-[28px] w-[28px] md:h-[32px] md:w-[32px] text-3xl font-normal uppercase flex items-center justify-center'>{userData?.firstname[0]}</div>
                     </button> :
                 <button className='mx-2 md:mx-4 p-2 bg-color2d rounded-full text-color1d border-2 border-color2d' onClick={() => onOpen()} type="button" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                     <span className="sr-only">Open user menu</span>

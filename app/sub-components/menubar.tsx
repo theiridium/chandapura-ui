1e91eaafc54d027ba0d46e2b690f3e6375bd59b0
priyankastro <priyankastro7@gmail.com>
2025-02-18 17:00:23 +0530

    fix(): header and menu

--- app/sub-components/menubar.tsx
+++ app/sub-components/menubar.tsx
@@ -8,50 +8,50 @@ const Menubar = (props: any) => {
         <Drawer
             isOpen={props.isOpen}
             onClose={props.onClose}
-            classNames={{ base: "bg-color1d text-white", closeButton: "text-color2d hover:text-color1d text-lg font-bold" }}
+            classNames={{ base: "bg-color1d text-white w-64", closeButton: "text-color1d lg:text-color2d hover:text-color1d text-lg font-bold hover:bg-white/50 bg-white/50 lg:bg-transparent !rounded-sm" }}
             size='sm'
             radius='none'>
             <DrawerContent>
                 {(onClose) => (
                     <>
                         <DrawerHeader className="flex flex-col gap-1 border-0">
-                            <div className='text-2xl mb-2 font-medium'>Welcome back</div>
-                            <div className='text-3xl font-medium'>{status === "authenticated" ? data?.user?.firstname : "Guest"} !</div>
+                            <div className='text-xl mb-2 font-medium'>Welcome back</div>
+                            <div className='text-2xl font-normal'>{status === "authenticated" ? data?.user?.firstname : "Guest"} !</div>
                         </DrawerHeader>
                         <DrawerBody>
-                            <ul className="text-2xl my-12 gap-10">
+                            <ul className="text-2xl my-8 gap-10 *:mb-8">
                                 {status === "unauthenticated" &&
-                                    <li className='mb-10'>
+                                    <li className=''>
                                         <a className='hover-underline' href={Resource.Login.link}>{Resource.Login.label}</a>
                                     </li>
                                 }
                                 {status === "authenticated" &&
                                     <>
-                                        {/* <li className='mb-10'>
+                                        {/* <li className=''>
                                             <a className='hover-underline' href={Resource.MyProfile.link}>{Resource.MyProfile.label}</a>
                                         </li> */}
-                                        {/* <li className='mb-10'>
+                                        {/* <li className=''>
                                             <a className='hover-underline' href={Resource.Dashboard.link}>{Resource.Dashboard.label}</a>
                                         </li> */}
                                     </>
                                 }
-                                <li className='mb-10'>
+                                <li className=''>
                                     <a className='hover-underline' href={Resource.Advertisement.dashboardLink}>{Resource.Advertisement.label}</a>
                                 </li>
-                                <li className='mb-10'>
+                                <li className=''>
                                     <a className='hover-underline' href={Resource.BusinessListing.dashboardLink}>{Resource.BusinessListing.label}</a>
                                 </li>
-                                <li className='mb-10'>
+                                <li className=''>
                                     <a className='hover-underline' href={Resource.PropertyListing.dashboardLink}>{Resource.PropertyListing.label}</a>
                                 </li>
-                                <li className='mb-10'>
+                                <li className=''>
                                     <a className='hover-underline' href={Resource.ClassifiedListing.dashboardLink}>{Resource.ClassifiedListing.label}</a>
                                 </li>
-                                <li className='mb-10'>
+                                <li className=''>
                                     <a className='hover-underline' href={Resource.JobListing.dashboardLink}>{Resource.JobListing.label}</a>
                                 </li>
                                 {status === "authenticated" &&
-                                    <li className='mb-10'>
+                                    <li className='mt-16'>
                                         <button className='bg-red-600 hover:bg-red-700 hover text-white py-2 px-4' onClick={() => signOut({ callbackUrl: '/' })}>{Resource.Logout.label}</button>
                                     </li>
                                 }
--- app/sub-components/user-profile-button.tsx
+++ app/sub-components/user-profile-button.tsx
@@ -14,15 +14,15 @@ const UserProfileButton = () => {
         <>
             {userData ?
                 !!userData.image ?
-                    <button className='mx-2 md:mx-4 border-2 border-color1d hover:border-color2d rounded-full' onClick={() => onOpen()} type="button" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                    <button className='border-2 border-color1d hover:border-color2d rounded-full' onClick={() => onOpen()} type="button" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                         <span className="sr-only">Open user menu</span>
                         <img src={userData.image} className='h-[44px] w-[44px] md:h-[48px] md:w-[48px] rounded-full' />
                     </button> :
-                    <button className='mx-2 md:mx-4 p-2 bg-color2d text-color1d border-2 border-color1d hover:border-color2d rounded-full' onClick={() => onOpen()} type="button" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                    <button className='p-2 bg-color2d text-color1d border-2 border-color1d hover:border-color2d rounded-full' onClick={() => onOpen()} type="button" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                         <span className="sr-only">Open user menu</span>
                         <div className='h-[28px] w-[28px] md:h-[32px] md:w-[32px] text-3xl font-normal uppercase flex items-center justify-center'>{userData?.firstname[0]}</div>
                     </button> :
-                <button className='mx-2 md:mx-4 p-2 bg-color2d rounded-full text-color1d border-2 border-color2d' onClick={() => onOpen()} type="button" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                <button className='p-2 bg-color2d rounded-full text-color1d border-2 border-color2d' onClick={() => onOpen()} type="button" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                     <span className="sr-only">Open user menu</span>
                     <User strokeWidth={2.2} fill='#ffc901' className='h-[24px] w-[24px]' />
                 </button>

23cd74e7e3e12158b6ae82746237d9ee653b239f
priyankastro <priyankastro7@gmail.com>
2025-03-06 06:25:35 +0530

    updates

--- app/components/modals/banner-guidline-modal.tsx
+++ app/components/modals/banner-guidline-modal.tsx
@@ -12,8 +12,9 @@ const BannerGuidlineModal = (props: any) => {
                             <img src='/images/Banner_Ad_Guidline_Desc.jpg' />
                             <ul className='list-disc mx-5 text-sm md:text-base mt-3'>
                                 <li>Ensure the minimum banner width to be 1536px with a height of 384px.</li>
+                                <li>The inner rectangle indicates the visibility range in mobile view.</li>
                                 <li>Please try to keep the main content in the center of banner within the width of 765px.</li>
-                                <li>Following this dimenssions correctly will provide a better visibility and reach to your target customers through our website.</li>
+                                <li>Following these dimenssions correctly would provide a better visibility and reach to your target customers through our website.</li>
                             </ul>
                         </ModalBody>
                         <ModalFooter>

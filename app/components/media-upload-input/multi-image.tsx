5bf5301d6a6c347b2c1963c6e31a58c0bf9c6f97
priyankastro <priyankastro7@gmail.com>
2025-04-25 04:03:26 +0530

    fix

--- app/components/media-upload-input/multi-image.tsx
+++ app/components/media-upload-input/multi-image.tsx
@@ -17,7 +17,7 @@ const MultiImage = ({ imageParams, uploadSuccess, setIsImagesInGallery, setEditM
     const [totalFiles, setTotalFiles] = useState(0);
     const onDrop = useCallback((acceptedFiles: any[]) => {
         setFiles(prevFiles => {
-            const fileNos = existingFiles.length + prevFiles.length + acceptedFiles.length;
+            const fileNos = (existingFiles?.length || 0) + prevFiles.length + acceptedFiles.length;
             if (fileNos > allowedNumber) {
                 toast.warning("You can only upload up to " + allowedNumber + " images.");
                 return prevFiles;

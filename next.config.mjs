855efb5a9481f332cec7000487e832dc31994a83
priyankastro <priyankastro7@gmail.com>
2025-05-14 05:59:48 +0530

    Migrated from Cloudinary to R2

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "res.cloudinary.com",
+        hostname: "assets.chandapura.com",
       },
     ],
   },
